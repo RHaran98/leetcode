5c0bf2300f1f40bbdfd24e19e629f567bf04630b
Haran Ravindran <haran@arrowhead.team>
2026-09-12 19:07:24 +0530

    Full soln to longest_repeating_character_replacement

--- src/longest_repeating_character_replacement.rs
+++ src/longest_repeating_character_replacement.rs
@@ -6,33 +6,38 @@ impl Solution {
         let sum_max = char_counts
             .iter()
             .fold((0, i32::MIN), |init, x| (init.0 + x, max(*x, init.1)));
-        sum_max.1 + min(sum_max.0 - sum_max.1, min(k, (j - i + 1) as i32))
+        let summer =sum_max.1 + min(sum_max.0 - sum_max.1, min(k, (j - i + 1) as i32));
+        if summer  == (j-i+1) as i32 {summer} else {0}
     }
     pub fn character_replacement(s: String, k: i32) -> i32 {
+        if k>=s.len() as i32 {
+            return  s.len() as i32;
+        }
         let s = s.as_bytes();
         let char_index = |x: u8| x as usize - b'A' as usize;
         let mut char_counts = [0; N_CHARS];
         let mut max_freq = 0;
         let mut max_str = 0;
         let mut prev_max_str = i32::MIN;
-        // char_counts[char_index(s[0])] = 1;
+        char_counts[char_index(s[0])] = 1;
 
         let (mut i, mut j) = (0, 0);
         loop {
             if (i + 1   > s.len() - (k as usize) || j + 1 > s.len()) {
                 break;
-            } else {
-                println!("cont");
-            }
+            } 
+            // else {
+            //     println!("cont");
+            // }
             // let idx = char_index(s[j]);
 
             // max_str = max(char_counts[idx].0,max_freq);
             let max_candidate = Self::get_max_char(char_counts, k, i, j);
             max_str = max(max_str, max_candidate);
-            println!(
-                "i:{} j:{} val_i {} val_j {} curr_max {} max_str {}, {} {}",
-                i, j, s[i], s[j], max_candidate, max_str, char_counts[0], char_counts[1]
-            );
+            // println!(
+            //     "i:{} j:{} val_i {} val_j {} curr_max {} max_str {}, {} {}",
+            //     i, j, s[i], s[j], max_candidate, max_str, char_counts[0], char_counts[1]
+            // );
             if (max_candidate > prev_max_str) {
                 j = j + 1;
                 if j + 1 <= s.len() {
@@ -57,9 +62,9 @@ impl Solution {
     }
 }
 fn main() {
-    let inp = String::from("AABABBA");
+    let inp = String::from("AAAAA");
     println!("Inp {:?}", inp);
 
-    let s = Solution::character_replacement(inp, 1);
+    let s = Solution::character_replacement(inp, 5);
     println!("Ans {:?}", s);
 }
